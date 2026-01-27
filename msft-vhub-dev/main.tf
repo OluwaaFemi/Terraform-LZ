b3da0ad0d4b6727f345498ea5e7acb51114e8975
Puneet V Devadiga <puneetvasu25@gmail.com>
2026-01-27 10:17:17 +0800

    Replace vhub terraform_remote_state with azurerm data sources

--- msft-vhub-dev/main.tf
+++ msft-vhub-dev/main.tf
@@ -4,25 +4,14 @@ resource "azurerm_resource_group" "hub" {
   tags     = var.tags
 }
 
-# Shared vWAN state (created by msft-vwan-prod stack)
-data "terraform_remote_state" "vwan" {
-  backend = "azurerm"
-  config = {
-    resource_group_name  = "msft-tfstate-prod-rg"
-    storage_account_name = "msfttfstateprod001"
-    container_name       = "tfstate"
-    key                  = "msft-lz-connectivity/msft-vwan-prod/terraform.tfstate"
-  }
+data "azurerm_virtual_wan" "vwan" {
+  name                = var.virtual_wan_name
+  resource_group_name = var.virtual_wan_resource_group_name
 }
 
-data "terraform_remote_state" "fwpolicy" {
-  backend = "azurerm"
-  config = {
-    resource_group_name  = "msft-tfstate-dev-rg"
-    storage_account_name = "msfttfstatedev001"
-    container_name       = "tfstate"
-    key                  = "msft-lz-connectivity/msft-fwpolicy-dev/terraform.tfstate"
-  }
+data "azurerm_firewall_policy" "fwpolicy" {
+  name                = var.firewall_policy_name
+  resource_group_name = var.firewall_policy_resource_group_name
 }
 
 module "connectivity_virtual_hub" {
@@ -35,7 +24,7 @@ module "connectivity_virtual_hub" {
       location            = var.location
       resource_group_name = azurerm_resource_group.hub.name
       address_prefix      = var.hub_address_prefix
-      virtual_wan_id      = data.terraform_remote_state.vwan.outputs.virtual_wan_id
+      virtual_wan_id      = data.azurerm_virtual_wan.vwan.id
       tags                = var.tags
     }
   }
@@ -53,7 +42,7 @@ resource "azurerm_firewall" "hub" {
     virtual_hub_id = module.connectivity_virtual_hub.resource["hub"].id
   }
 
-  firewall_policy_id = data.terraform_remote_state.fwpolicy.outputs.firewall_policy_id
+  firewall_policy_id = data.azurerm_firewall_policy.fwpolicy.id
 
   tags = var.tags
 }
--- msft-vhub-prod/variables.tf
+++ msft-vhub-prod/variables.tf
@@ -16,6 +16,18 @@ variable "virtual_hub_name" {
   default     = "msft-vhub-prod"
 }
 
+variable "virtual_wan_resource_group_name" {
+  description = "Resource group name that contains the shared Virtual WAN (created by the VWAN stack)."
+  type        = string
+  default     = "msft-prod-connectivity-rg"
+}
+
+variable "virtual_wan_name" {
+  description = "Name of the shared Virtual WAN (created by the VWAN stack)."
+  type        = string
+  default     = "msft-prod-sea-vwan"
+}
+
 variable "hub_address_prefix" {
   description = "Address prefix for the hub (CIDR)."
   type        = string
@@ -26,6 +38,18 @@ variable "firewall_name" {
   description = "Name for the Azure Firewall deployed into the secured vHub."
   type        = string
   default     = "msft-vhub-prod-firewall"
+}
+
+variable "firewall_policy_resource_group_name" {
+  description = "Resource group name that contains the Azure Firewall Policy to attach to the secured hub firewall."
+  type        = string
+  default     = "msft-vhub-prod-rg"
+}
+
+variable "firewall_policy_name" {
+  description = "Azure Firewall Policy name to attach to the secured hub firewall."
+  type        = string
+  default     = "msft-vhub-prod-firewall-policy"
 }
 
 variable "enable_module_telemetry" {
